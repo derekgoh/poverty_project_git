4b2db0e04b2d2d48a0941a89a1a56b8b681fb50c
derekgoh <derekgjy@gmail.com>
2018-11-15 14:24:49 -0500

    add tidyverse

--- appmultiple_git.R
+++ appmultiple_git.R
@@ -8,7 +8,7 @@ library(tools)
 library(DT)
 library(plotly)
 library(shinydashboard)
-library(foreign)
+library(tidyverse)
 
 baseline <- read.csv("baseline.publicuse.oct17.csv")
 f3 <- read.csv("followup3.csv")
