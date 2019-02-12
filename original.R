1e88f689a5c5bb483ff4eacf0faad559a71b6270
derekgoh <derekgjy@gmail.com>
2019-02-12 16:35:48 -0500

    stacked bar charts

--- original.R
+++ original.R
@@ -15,6 +15,15 @@ library(psych)
 library(gmodels)
 
 edited <- read.csv("for_Derek.csv")
+baseline <- read.csv("baseline.publicuse.oct17.csv")
+f3 <- read.csv("followup3.csv")
+f6 <- read.csv("followup6.csv")
+f9 <- read.csv("followup9.csv")
+f12 <- read.csv("followup12.csv")
+f15 <- read.csv("followup15.csv")
+f18 <- read.csv("followup18.csv")
+f21 <- read.csv("followup21.csv")
+f24 <- read.csv("followup24.csv")
 
 #UI
 ui <- navbarPage("Poverty Tracker Data", windowTitle = "Poverty Tracker Data", theme = shinytheme ("cosmo"), 
@@ -54,9 +63,46 @@ ui <- navbarPage("Poverty Tracker Data", windowTitle = "Poverty Tracker Data", t
                                                                 actionButton("reset", "Clear", style = "color: #fff; background-color: #337ab7; border-color: #2e6da4"),
                                                                 br(), br(), 
                                                                 verbatimTextOutput(outputId = "table"))
+                                                      ),
+                                                      tabPanel(title = "Datasets", sidebarPanel(width = 4,
+                                                                                                h2("Download Data"),
+                                                                                                HTML("Select filetype, dataset and variables, then hit 'Download Data'"), 
+                                                                                                br(), br(),
+                                                                                                radioButtons(inputId = "filetype",
+                                                                                                             label = "Filetype:",
+                                                                                                             choices = "csv",
+                                                                                                             selected = "csv"),
+                                                                                                
+                                                                                                radioButtons(inputId = "source", 
+                                                                                                             label = "Dataset:", 
+                                                                                                             choices = c(
+                                                                                                               "Baseline" = "baseline", 
+                                                                                                               "Adult and Child Health and Wellbeing" = "f3", 
+                                                                                                               "Neighborhoods and Service Utilization" = "f6", 
+                                                                                                               "Assets and Debts" = "f9", 
+                                                                                                               "First Annual Core Follow-Up" = "f12", 
+                                                                                                               "Consumer Expenditures" = "f15", 
+                                                                                                               "Employment and Job Search" = "f18", 
+                                                                                                               "Adult and Child Health and Wellbeing, Immigration History" = "f21", 
+                                                                                                               "Second Annual Core Follow-up" = "f24"), 
+                                                                                                             selected = "baseline"), 
+                                                                                                
+                                                                                                uiOutput("colnames"),
+                                                                                                downloadButton(outputId = "download_data", label = "Download Data")
+                                                      ),
+                                                      
+                                                      mainPanel(width = 8,
+                                                                br(), br(),
+                                                                HTML ("Select the relevant variables on the left"),
+                                                                br(), br(), 
+                                                                DT::dataTableOutput(outputId = "povertytable"), 
+                                                                br(), br(), 
+                                                                HTML ("For a more detailed explanation of each variable, please refer to the codebook below:"),
+                                                                br(), br(), 
+                                                                downloadButton(outputId = "codebook", label = "Download codebook"))
                                                       )
-                                                      )
-                          ), 
+                 )
+                 ), 
                                                       
                  tabPanel("About", sidebarPanel(width = 4, tags$img(src = "graphic.png", width = "85%", height = "85%", style = "display:block; margin-left:auto; margin-right: auto;")
                  ), 
@@ -77,6 +123,36 @@ server <- function(input, output, session) {
   # x and y as reactive expressions
   x <- reactive({ toTitleCase(str_replace_all(input$x, "_", " ")) })
   y <- reactive({ toTitleCase(str_replace_all(input$y, "_", " ")) })
+  
+  #Reactive dataset variable 
+  data_source <- reactive ({
+    if(input$source == "baseline") {
+      data <- baseline 
+    } else if (input$source == "f3") {
+      data <- f3
+    } else if (input$source == "f6") {
+      data <- f6
+    } else if (input$source == "f9") {
+      data <- f9
+    } else if (input$source == "f12") {
+      data <- f12
+    } else if (input$source == "f15") {
+      data <- f15
+    } else if (input$source == "f18") {
+      data <- f18
+    } else if (input$source == "f21") {
+      data <- f21
+    } else if (input$source == "f24") {
+      data <- f24
+    }
+    return(data)
+  })
+  
+  #Col Names 
+  output$colnames <- renderUI ({
+    names <- colnames(data_source())
+    checkboxGroupInput("selected_var", "Variables:", names, "subject_id")
+  })
   
   # Enter text for plot title
   output$title <- renderUI ({
@@ -107,14 +183,27 @@ server <- function(input, output, session) {
       mutate(b = format(round(meanY, digits = 2), nsmall = 2))
   })
   
+  #Data Cleaning for Stacked Bar Chart 
+  edited_stackbar <- reactive ({
+    edited %>%
+      group_by_(input$x, input$y) %>% 
+      summarize(Percentage = n()) %>%
+      group_by_(input$x) %>%
+      mutate(Percentage = Percentage / sum(Percentage) * 100) %>%
+      arrange_(input$x) %>%
+      mutate(label_pos1 = cumsum(Percentage) - Percentage / 2,
+             perc_text1 = paste0(round(Percentage), "%"))
+  })
+  
   # Create plot
   output$plot <- renderPlot({
     if ((input$x == "spmres" & input$y == "imp_age") | (input$x == "imp_age" & input$y == "spmres")) {
       ggplot(edited, aes_string(x = input$x, y = input$y)) +
-        geom_point() + 
+        geom_point() +
         labs(x = x(),
              y = y(),
-             title = toTitleCase(input$plot_title))
+             title = toTitleCase(input$plot_title)) +
+        geom_smooth(method = "lm")
     } else if ((input$x == "spmpov" & input$y == "sevhard") | (input$x == "spmpov" & input$y == "sevhealthd")
                | (input$x == "spmpov" & input$y == "imp_female") | (input$x == "sevhard" & input$y == "spmpov") 
                | (input$x == "sevhard" & input$y == "sevhealthd") | (input$x == "sevhard" & input$y == "imp_female")
@@ -129,6 +218,15 @@ server <- function(input, output, session) {
         facet_wrap( ~get(input$x)) + 
         labs(title = toTitleCase(input$plot_title)) +
         theme_void()
+    } else if ((input$x == "spmpov" & input$y == "imp_educat") | (input$x == "sevhard" & input$y == "imp_educat") 
+               | (input$x == "sevhealthd" & input$y == "imp_educat") | (input$x == "imp_female" & input$y == "imp_educat")) {
+      ggplot(data = edited_stackbar(), aes_string(x = input$x, y = "Percentage", fill = input$y)) +
+        geom_bar(stat = "identity", width = 0.5) +
+        geom_text(aes(label = perc_text1), position = position_stack(vjust = 0.5)) +
+        scale_fill_distiller(palette = "Oranges") +
+        labs(x = x(),
+             y = y(),
+             title = toTitleCase(input$plot_title))
     } else {
       ggplot(data = edited_bar(), aes_string(x = input$x, y = "meanY")) +
         geom_bar(stat = "identity", fill = "lightsalmon2", width = 0.5) +
@@ -161,6 +259,36 @@ server <- function(input, output, session) {
   observeEvent(input$reset, {
     output$table <- NULL
     })
+  
+  # Print data table
+  output$povertytable <- DT::renderDataTable(
+    DT::datatable(data = data_source()[input$selected_var],
+                  options = list(pageLength = 10), 
+                  rownames = FALSE)
+  )
+  
+  # Download codebook
+  output$codebook <- downloadHandler(
+    filename = function () {
+      paste0("All_Codebooks", ".zip", sep = "")
+    },
+    content = function(file) {
+      file.copy("codebooks.zip", file)
+    }, 
+    contentType = "application/zip"
+  )
+  
+  # Download data
+  output$download_data <- downloadHandler(
+    filename = function() {
+      paste0(input$source, ".csv", sep = "")
+    },
+    content = function(file) { 
+      if(input$filetype == "csv"){ 
+        write_csv(data_source() %>% select(input$selected_var), path = file) 
+      }
+    }
+  )
 }
 
 # Create a Shiny app object
